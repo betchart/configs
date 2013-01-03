15db9359f20ec654ea6e879b2765eaba6ccdec5f
Burton Betchart <betchart@gmail.com>
2013-01-02 19:58:33 -0500

    update configs

--- etc/acpi/bluetooth.sh
+++ etc/acpi/bluetooth.sh
@@ -3,17 +3,17 @@
 state=`grep status /proc/acpi/ibm/bluetooth | cut -f2 -d":"`
 if [ $state = "enabled" ] 
 then
-    /usr/bin/mpc enable 1
-    /usr/bin/mpc disable 2
+    #/usr/bin/mpc enable 1
+    #/usr/bin/mpc disable 2
     /etc/init.d/bluetooth stop
     killall bluetoothd
-    echo "disable" > /proc/acpi/ibm/bluetooth
+    rfkill block bluetooth
 else
-    echo "enable" > /proc/acpi/ibm/bluetooth
+    rfkill unblock bluetooth
     /etc/init.d/bluetooth start
-    /usr/bin/mpc pause
-    /usr/bin/mpc enable 2
-    /usr/bin/mpc disable 1
-    sleep 2
-    /usr/bin/mpc play
+    #/usr/bin/mpc pause
+    #/usr/bin/mpc enable 2
+    #/usr/bin/mpc disable 1
+    #sleep 2
+    #/usr/bin/mpc play
 fi
